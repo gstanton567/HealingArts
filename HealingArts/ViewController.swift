d74e7f043b3f46ba779579f2f16907372062b942
bfehr11 <bfehr11@gmail.com>
2019-06-07 10:56:27 -0500

    Initial Commit

---
+++ HealingArts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealingArts
+//
+//  Created by Brady Fehr on 6/7/19.
+//  Copyright © 2019 Brady Fehr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
