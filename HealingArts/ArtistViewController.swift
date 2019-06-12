1579a68555f1473454afdf06556dd7d06da9551e
Justine Linscott <justinelinscott@unomaha.edu>
2019-06-12 11:34:00 -0500

    add name of artists

--- HealingArts/ArtistViewController.swift
+++ HealingArts/ArtistViewController.swift
@@ -16,12 +16,21 @@ class ArtistViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var tableView: UITableView!
     
 //properties
-    //var artistName : [Artist] = []
-    //var artistImage : [Artist] = []
+    var artworks : [Artwork] = [Artwork]()
     var images = ["chihulypic", "harnoor", "dan", "gold", "kaneko"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Artists"
+        Firebase.getAllDocumentsInCollection { (artworks, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                self.artworks = artworks
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
 //table view functions
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -29,14 +38,15 @@ class ArtistViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return images.count
+        return artworks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
-        let pictures = images[indexPath.row]
-        cell?.imageView!.image = UIImage(named: pictures)
-        cell?.textLabel?.text = "artist"
+      //  let pictures = [artworks[indexPath.row].imageURLs]
+        let name = artworks[indexPath.row].artist
+      //  cell?.imageView!.image = UIImage(named: arraypic)
+        cell?.textLabel?.text = name
         return cell!
     }
 
