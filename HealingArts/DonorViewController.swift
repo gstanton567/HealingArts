fb1416051a4a92d684622753addb2b5a090e77cd
Gwyneth Semanisin <gesemanisin@gmail.com>
2019-06-12 11:50:22 -0500

    donor page is prettier

--- HealingArts/DonorViewController.swift
+++ HealingArts/DonorViewController.swift
@@ -12,6 +12,7 @@ class DonorViewController: UIViewController {
 
     @IBOutlet weak var donorsTextView: UITextView!
     
+    @IBOutlet weak var thanksLabel: UILabel!
     //  Healing Arts Program donors
     let donors = ["Pamela and Fred Buffett","Suzanne & Walter Scott", "Warren Buffett", "and Others"]
     
@@ -20,10 +21,14 @@ class DonorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        thanksLabel.text = "Would Like To Thank Our \nGenerous Donors"
+        
         let thankYouList = donors.joined(separator: "\n")
         
         //all donors will be printed on a new line within the textView
         donorsTextView.text = thankYouList
+        
+        view.backgroundColor = UIColor.ChihulyUI.Blue.DeepAqua
     }
     
 
