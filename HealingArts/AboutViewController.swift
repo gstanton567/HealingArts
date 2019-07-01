e54e1e5ae88278f89c1caad626ce2f66aa79607f
Carly Cameron <carlycameron01@gmail.com>
2019-07-01 08:47:27 -0500

    about page

--- HealingArts/AboutViewController.swift
+++ HealingArts/AboutViewController.swift
@@ -24,89 +24,89 @@ class AboutViewController: UIViewController, SFSafariViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = UIColor.ChihulyUI.Red.UNMC
-        
-        view.layer.masksToBounds = true
-        view.layer.borderWidth = 3
-        view.layer.borderColor = UIColor.ChihulyCG.Red.UNMC
-        
-        //titleLabel prettiness thing additions, idk
-        titleLabel.text = "About the Healing Arts Program"
-        titleLabel.textColor = UIColor.white
-        titleLabel.layer.masksToBounds = true
-        
-        titleLabel.layer.borderWidth = 3
-        titleLabel.layer.borderColor = UIColor.white.cgColor
-        titleLabel.layer.backgroundColor = UIColor.ChihulyCG.Red.UNMC
-        
-        
-        //infoTextView prettiness thing additions, idk
-        infoTextView.layer.masksToBounds = true
-        infoTextView.textColor = UIColor.white
-        
-        
-        //infoButton prettiness thing additions, idk
-        infoButton.layer.masksToBounds = true
-        infoButton.layer.cornerRadius = 15
-        infoButton.layer.borderWidth = 3
-        infoButton.setTitle("More Info", for: .normal)
-        infoButton.setTitleColor(UIColor.white, for: .normal)
-        infoButton.layer.borderColor = UIColor.white.cgColor
-        infoButton.backgroundColor = UIColor.ChihulyUI.Red.FlowerEdgeRed
-        
-        //words from artists button
-        artistsWordsButton.layer.masksToBounds = true
-        artistsWordsButton.layer.cornerRadius = 15
-        artistsWordsButton.layer.borderWidth = 3
-        artistsWordsButton.layer.borderColor = UIColor.white.cgColor
-        artistsWordsButton.backgroundColor = UIColor.ChihulyUI.Red.FlowerEdgeRed
-        artistsWordsButton.setTitleColor(UIColor.white, for: .normal)
-        
-        //imageView prettiness aesthetics I guess, idk
-        aboutPageInfoImageView.image = UIImage(named: "CancerCenter")
-        
-        aboutPageInfoImageView.layer.borderWidth = 3
-        aboutPageInfoImageView.layer.borderColor = UIColor.white.cgColor
-        aboutPageInfoImageView.backgroundColor = UIColor.ChihulyUI.Red.UNMCSafariBackground
-        
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        infoTextView.setContentOffset(CGPoint.zero, animated: false)
-    }
-    
-    @IBAction func artistWordsButtonIsPressed(_ sender: UIButton) {
-        if let path = Bundle.main.path(forResource: "thevideo", ofType: "mp4"){
-            let video = AVPlayer(url: URL(fileURLWithPath: path))
-            let videoPlayer = AVPlayerViewController()
-            videoPlayer.player = video
-            present(videoPlayer, animated: true, completion:{
-                video.play()
-            })
-        }
-    }
-    
-    
-    
-    @IBAction func onInfoButtonPressed(_ sender: UIButton) {
-        if let link = URL(string: linkString) {
-            let sfvc = SFSafariViewController(url: link)
-            sfvc.delegate = (self as! SFSafariViewControllerDelegate)
-            sfvc.preferredControlTintColor = .white
-            sfvc.preferredBarTintColor = UIColor.ChihulyUI.Red.UNMCSafariBackground
-            
-            present(sfvc, animated: true)
-            
-        }
+//
+//        view.backgroundColor = UIColor.ChihulyUI.Red.UNMC
+//
+//        view.layer.masksToBounds = true
+//        view.layer.borderWidth = 3
+//        view.layer.borderColor = UIColor.ChihulyCG.Red.UNMC
+//
+//        //titleLabel prettiness thing additions, idk
+//        titleLabel.text = "About the Healing Arts Program"
+//        titleLabel.textColor = UIColor.white
+//        titleLabel.layer.masksToBounds = true
+//
+//        titleLabel.layer.borderWidth = 3
+//        titleLabel.layer.borderColor = UIColor.white.cgColor
+//        titleLabel.layer.backgroundColor = UIColor.ChihulyCG.Red.UNMC
+//
+//
+//        //infoTextView prettiness thing additions, idk
+//        infoTextView.layer.masksToBounds = true
+//        infoTextView.textColor = UIColor.white
+//
+//
+//        //infoButton prettiness thing additions, idk
+//        infoButton.layer.masksToBounds = true
+//        infoButton.layer.cornerRadius = 15
+//        infoButton.layer.borderWidth = 3
+//        infoButton.setTitle("More Info", for: .normal)
+//        infoButton.setTitleColor(UIColor.white, for: .normal)
+//        infoButton.layer.borderColor = UIColor.white.cgColor
+//        infoButton.backgroundColor = UIColor.ChihulyUI.Red.FlowerEdgeRed
+//
+//        //words from artists button
+//        artistsWordsButton.layer.masksToBounds = true
+//        artistsWordsButton.layer.cornerRadius = 15
+//        artistsWordsButton.layer.borderWidth = 3
+//        artistsWordsButton.layer.borderColor = UIColor.white.cgColor
+//        artistsWordsButton.backgroundColor = UIColor.ChihulyUI.Red.FlowerEdgeRed
+//        artistsWordsButton.setTitleColor(UIColor.white, for: .normal)
+//
+//        //imageView prettiness aesthetics I guess, idk
+//        aboutPageInfoImageView.image = UIImage(named: "CancerCenter")
+//
+//        aboutPageInfoImageView.layer.borderWidth = 3
+//        aboutPageInfoImageView.layer.borderColor = UIColor.white.cgColor
+//        aboutPageInfoImageView.backgroundColor = UIColor.ChihulyUI.Red.UNMCSafariBackground
         
     }
     
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-        dismiss(animated: true)
-    }
-    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        infoTextView.setContentOffset(CGPoint.zero, animated: false)
+//    }
+//
+//    @IBAction func artistWordsButtonIsPressed(_ sender: UIButton) {
+//        if let path = Bundle.main.path(forResource: "thevideo", ofType: "mp4"){
+//            let video = AVPlayer(url: URL(fileURLWithPath: path))
+//            let videoPlayer = AVPlayerViewController()
+//            videoPlayer.player = video
+//            present(videoPlayer, animated: true, completion:{
+//                video.play()
+//            })
+//        }
+//    }
+//
+//
+//
+//    @IBAction func onInfoButtonPressed(_ sender: UIButton) {
+//        if let link = URL(string: linkString) {
+//            let sfvc = SFSafariViewController(url: link)
+//            sfvc.delegate = (self as! SFSafariViewControllerDelegate)
+//            sfvc.preferredControlTintColor = .white
+//            sfvc.preferredBarTintColor = UIColor.ChihulyUI.Red.UNMCSafariBackground
+//
+//            present(sfvc, animated: true)
+//
+//        }
+//
+//    }
+//
+//    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+//        dismiss(animated: true)
+//    }
+//
 }
 
 
