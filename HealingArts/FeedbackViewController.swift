4b3fdffd164bb7510c60a19d9e51226b0931bca6
Justine Linscott <justinelinscott@unomaha.edu>
2019-07-02 13:24:13 -0500

    feedback

--- HealingArts/FeedbackViewController.swift
+++ HealingArts/FeedbackViewController.swift
@@ -12,17 +12,24 @@ import Firebase
 
 class FeedbackViewController: UIViewController {
    
-    @IBOutlet weak var starOneImageView: UIImageView!
-    @IBOutlet weak var starTwoImageView: UIImageView!
-    @IBOutlet weak var starThreeImageView: UIImageView!
-    @IBOutlet weak var starFourImageView: UIImageView!
-    @IBOutlet weak var starFiveImageView: UIImageView!
+//    @IBOutlet weak var starOneImageView: UIImageView!
+//    @IBOutlet weak var starTwoImageView: UIImageView!
+//    @IBOutlet weak var starThreeImageView: UIImageView!
+//    @IBOutlet weak var starFourImageView: UIImageView!
+//    @IBOutlet weak var starFiveImageView: UIImageView!
     
     @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var interestingLabel: UILabel!
+    @IBOutlet weak var qualityLabel: UILabel!
+    @IBOutlet weak var rateSlider: UISlider!
+    @IBOutlet weak var qualitySlider: UISlider!
+    @IBOutlet weak var interestingSlider: UISlider!
     
-    @IBOutlet var nameTextField: UITextField!
+//    @IBOutlet var nameTextField: UITextField!
+//
+//    @IBOutlet var emailTextField: UITextField!
     
-    @IBOutlet var emailTextField: UITextField!
     
     @IBOutlet var textView: UITextView!
     
@@ -35,108 +42,123 @@ class FeedbackViewController: UIViewController {
         textView.layer.cornerRadius = 5
         textView.layer.borderColor = UIColor.ChihulyCG.Gray.Graphite
         textView.layer.borderWidth = 1
-        nameTextField.layer.borderColor = UIColor.ChihulyCG.Gray.Graphite
-        nameTextField.layer.borderWidth = 1
-        emailTextField.layer.borderColor = UIColor.ChihulyCG.Gray.Graphite
-        emailTextField.layer.borderWidth = 1
         
-        let tapGestureRecognizerOne = UITapGestureRecognizer(target: self, action: #selector(starOneTapped(tapGestureRecognizer:)))
-        starOneImageView.isUserInteractionEnabled = true
-        starOneImageView.addGestureRecognizer(tapGestureRecognizerOne)
-        
-        let tapGestureRecognizerTwo = UITapGestureRecognizer(target: self, action: #selector(starTwoTapped(tapGestureRecognizer:)))
-        starTwoImageView.isUserInteractionEnabled = true
-        starTwoImageView.addGestureRecognizer(tapGestureRecognizerTwo)
-        
-        let tapGestureRecognizerThree = UITapGestureRecognizer(target: self, action: #selector(starThreeTapped(tapGestureRecognizer:)))
-        starThreeImageView.isUserInteractionEnabled = true
-        starThreeImageView.addGestureRecognizer(tapGestureRecognizerThree)
-        
-        let tapGestureRecognizerFour = UITapGestureRecognizer(target: self, action: #selector(starFourTapped(tapGestureRecognizer:)))
-        starFourImageView.isUserInteractionEnabled = true
-        starFourImageView.addGestureRecognizer(tapGestureRecognizerFour)
-        
-        let tapGestureRecognizerFive = UITapGestureRecognizer(target: self, action: #selector(starFiveTapped(tapGestureRecognizer:)))
-        starFiveImageView.isUserInteractionEnabled = true
-        starFiveImageView.addGestureRecognizer(tapGestureRecognizerFive)
+//        nameTextField.layer.borderColor = UIColor.ChihulyCG.Gray.Graphite
+//        nameTextField.layer.borderWidth = 1
+//        emailTextField.layer.borderColor = UIColor.ChihulyCG.Gray.Graphite
+//        emailTextField.layer.borderWidth = 1
+//
+//        let tapGestureRecognizerOne = UITapGestureRecognizer(target: self, action: #selector(starOneTapped(tapGestureRecognizer:)))
+//        starOneImageView.isUserInteractionEnabled = true
+//        starOneImageView.addGestureRecognizer(tapGestureRecognizerOne)
+//
+//        let tapGestureRecognizerTwo = UITapGestureRecognizer(target: self, action: #selector(starTwoTapped(tapGestureRecognizer:)))
+//        starTwoImageView.isUserInteractionEnabled = true
+//        starTwoImageView.addGestureRecognizer(tapGestureRecognizerTwo)
+//
+//        let tapGestureRecognizerThree = UITapGestureRecognizer(target: self, action: #selector(starThreeTapped(tapGestureRecognizer:)))
+//        starThreeImageView.isUserInteractionEnabled = true
+//        starThreeImageView.addGestureRecognizer(tapGestureRecognizerThree)
+//
+//        let tapGestureRecognizerFour = UITapGestureRecognizer(target: self, action: #selector(starFourTapped(tapGestureRecognizer:)))
+//        starFourImageView.isUserInteractionEnabled = true
+//        starFourImageView.addGestureRecognizer(tapGestureRecognizerFour)
+//
+//        let tapGestureRecognizerFive = UITapGestureRecognizer(target: self, action: #selector(starFiveTapped(tapGestureRecognizer:)))
+//        starFiveImageView.isUserInteractionEnabled = true
+//        starFiveImageView.addGestureRecognizer(tapGestureRecognizerFive)
         
     }
     
-    @objc func starOneTapped(tapGestureRecognizer: UITapGestureRecognizer)
-    {
-        starOneImageView.image = UIImage.init(named: "RedStar")
-        starTwoImageView.image = UIImage.init(named: "RedStarHollow")
-        starThreeImageView.image = UIImage.init(named: "RedStarHollow")
-        starFourImageView.image = UIImage.init(named: "RedStarHollow")
-        starFiveImageView.image = UIImage.init(named: "RedStarHollow")
-        // Your action
+    @IBAction func rateSliderAction(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        rateLabel.text = "\(currentValue)"
     }
     
-    @objc func starTwoTapped(tapGestureRecognizer: UITapGestureRecognizer)
-    {
-        starOneImageView.image = UIImage.init(named: "RedStar")
-        starTwoImageView.image = UIImage.init(named: "RedStar")
-        starThreeImageView.image = UIImage.init(named: "RedStarHollow")
-        starFourImageView.image = UIImage.init(named: "RedStarHollow")
-        starFiveImageView.image = UIImage.init(named: "RedStarHollow")
-        // Your action
+    @IBAction func qualitySliderAction(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        qualityLabel.text = "\(currentValue)"
     }
     
-    @objc func starThreeTapped(tapGestureRecognizer: UITapGestureRecognizer)
-    {
-        starOneImageView.image = UIImage.init(named: "RedStar")
-        starTwoImageView.image = UIImage.init(named: "RedStar")
-        starThreeImageView.image = UIImage.init(named: "RedStar")
-        starFourImageView.image = UIImage.init(named: "RedStarHollow")
-        starFiveImageView.image = UIImage.init(named: "RedStarHollow")
-        // Your action
+    @IBAction func interestingSliderAction(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        interestingLabel.text = "\(currentValue)"
     }
-    
-    @objc func starFourTapped(tapGestureRecognizer: UITapGestureRecognizer)
-    {
-        starOneImageView.image = UIImage.init(named: "RedStar")
-        starTwoImageView.image = UIImage.init(named: "RedStar")
-        starThreeImageView.image = UIImage.init(named: "RedStar")
-        starFourImageView.image = UIImage.init(named: "RedStar")
-        starFiveImageView.image = UIImage.init(named: "RedStarHollow")
-        // Your action
-    }
-    
-    @objc func starFiveTapped(tapGestureRecognizer: UITapGestureRecognizer)
-    {
-        starOneImageView.image = UIImage.init(named: "RedStar")
-        starTwoImageView.image = UIImage.init(named: "RedStar")
-        starThreeImageView.image = UIImage.init(named: "RedStar")
-        starFourImageView.image = UIImage.init(named: "RedStar")
-        starFiveImageView.image = UIImage.init(named: "RedStar")
-        // Your action
-    }
+    //    @objc func starOneTapped(tapGestureRecognizer: UITapGestureRecognizer)
+//    {
+//        starOneImageView.image = UIImage.init(named: "RedStar")
+//        starTwoImageView.image = UIImage.init(named: "RedStarHollow")
+//        starThreeImageView.image = UIImage.init(named: "RedStarHollow")
+//        starFourImageView.image = UIImage.init(named: "RedStarHollow")
+//        starFiveImageView.image = UIImage.init(named: "RedStarHollow")
+//        // Your action
+//    }
+//
+//    @objc func starTwoTapped(tapGestureRecognizer: UITapGestureRecognizer)
+//    {
+//        starOneImageView.image = UIImage.init(named: "RedStar")
+//        starTwoImageView.image = UIImage.init(named: "RedStar")
+//        starThreeImageView.image = UIImage.init(named: "RedStarHollow")
+//        starFourImageView.image = UIImage.init(named: "RedStarHollow")
+//        starFiveImageView.image = UIImage.init(named: "RedStarHollow")
+//        // Your action
+//    }
+//
+//    @objc func starThreeTapped(tapGestureRecognizer: UITapGestureRecognizer)
+//    {
+//        starOneImageView.image = UIImage.init(named: "RedStar")
+//        starTwoImageView.image = UIImage.init(named: "RedStar")
+//        starThreeImageView.image = UIImage.init(named: "RedStar")
+//        starFourImageView.image = UIImage.init(named: "RedStarHollow")
+//        starFiveImageView.image = UIImage.init(named: "RedStarHollow")
+//        // Your action
+//    }
+//
+//    @objc func starFourTapped(tapGestureRecognizer: UITapGestureRecognizer)
+//    {
+//        starOneImageView.image = UIImage.init(named: "RedStar")
+//        starTwoImageView.image = UIImage.init(named: "RedStar")
+//        starThreeImageView.image = UIImage.init(named: "RedStar")
+//        starFourImageView.image = UIImage.init(named: "RedStar")
+//        starFiveImageView.image = UIImage.init(named: "RedStarHollow")
+//        // Your action
+//    }
+//
+//    @objc func starFiveTapped(tapGestureRecognizer: UITapGestureRecognizer)
+//    {
+//        starOneImageView.image = UIImage.init(named: "RedStar")
+//        starTwoImageView.image = UIImage.init(named: "RedStar")
+//        starThreeImageView.image = UIImage.init(named: "RedStar")
+//        starFourImageView.image = UIImage.init(named: "RedStar")
+//        starFiveImageView.image = UIImage.init(named: "RedStar")
+//        // Your action
+//    }
     
     @IBAction func onSubmitPressed(_ sender: UIButton) {
         let databaseRef  = Firestore.firestore()
         
         //Text fields cannot be blank or nil
-        if nameTextField.text != nil && emailTextField.text != nil && textView.text != nil && nameTextField.text != "" && emailTextField.text != "" && textView.text != "" {
-            var ref = databaseRef.collection("Feedback").addDocument(data: ["name" : nameTextField.text, "email" : emailTextField.text, "comment" : textView.text]) {err in
-                if let err = err {
-                    print("Error adding document: \(err)")
-                } else {
+//        if nameTextField.text != nil && emailTextField.text != nil && textView.text != nil && nameTextField.text != "" && emailTextField.text != "" && textView.text != "" {
+//            var ref = databaseRef.collection("Feedback").addDocument(data: ["name" : nameTextField.text, "email" : emailTextField.text, "comment" : textView.text]) {err in
+//                if let err = err {
+//                    print("Error adding document: \(err)")
+//                } else {
                     let alertController = UIAlertController(title: "Thank You!", message: "We have received your feedback", preferredStyle: .alert)
                     let okay = UIAlertAction(title: "Okay", style: .default, handler: { (UIAlertAction) in
-                        self.nameTextField.text = ""
-                        self.emailTextField.text = ""
+//                        self.nameTextField.text = ""
+//                        self.emailTextField.text = ""
                         self.textView.text = ""
                     })
                     alertController.addAction(okay)
                     self.present(alertController, animated: true)
-                }
-            }
-        }
-        else {
-            let alertController = UIAlertController(title: "Error", message: "Please fill in the text fields before submitting", preferredStyle: .alert)
-            let okay = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alertController.addAction(okay)
-            present(alertController, animated: true)
+        
+//            }
+//        }
+//        else {
+//            let alertController = UIAlertController(title: "Error", message: "Please fill in the text fields before submitting", preferredStyle: .alert)
+//            let okay = UIAlertAction(title: "OK", style: .default, handler: nil)
+//            alertController.addAction(okay)
+//            present(alertController, animated: true)
         }
     }
     
@@ -150,4 +172,4 @@ class FeedbackViewController: UIViewController {
     }
     */
 
-}
+
