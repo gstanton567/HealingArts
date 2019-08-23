be9725f369dc3cf43b6e5dfd8e3befc8aaf63c64
Grayson Stanton <gstanton@unomaha.edu>
2019-08-22 20:07:37 -0500

    made webiste buttons work

--- HealingArts/Firebase.swift
+++ HealingArts/Firebase.swift
@@ -137,7 +137,7 @@ class Firebase {
                     let data = document.data()
                     if let urlStrings = data["images"] as? [String] {
                         NetworkManager.getImagesWith(urlStrings: urlStrings, completion: { (images) in
-                            let newArtist = Artist(name: data["name"] as! String, textDesc: data["textDesc"] as! String, images: images)
+                            let newArtist = Artist(name: data["name"] as! String, textDesc: data["textDesc"] as! String, images: images, website: data["website"] as! String)
                             artists.append(newArtist)
                             if artists.count == querySnapshot!.documents.count {
                                 completion(artists, nil)
