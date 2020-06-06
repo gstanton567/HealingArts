775958f80bd686b3e10e99b65b6fb17e1c439511
Carly Cameron <cmcameron@unomaha.edu>
2020-06-05 22:42:53 -0500

    fix

--- HealingArts/Parallax/ParallaxCollectionViewController.swift
+++ HealingArts/Parallax/ParallaxCollectionViewController.swift
@@ -53,7 +53,7 @@ class ParallaxCollectionViewController: UICollectionViewController, UICollection
         var imageSizeMultiplier = 1.0
         if (artworksMod[indexPath.item].title == "Autumn Leaves Persian Installation")
         {
-            imageSizeMultiplier = 3
+            imageSizeMultiplier = 2.5
         }
         if (artworksMod[indexPath.item].title == "The Hope Tapestry")
         {
@@ -86,7 +86,7 @@ class ParallaxCollectionViewController: UICollectionViewController, UICollection
         for artwork in Firebase.globalArtworks{
             isDuplicate = false
             for modArtwork in artworksMod{
-                if (modArtwork.textDescription == artwork.textDescription && artwork.title == "Niijima Floats") {
+                if (modArtwork.textDescription == artwork.textDescription && (artwork.title == "Niijima Floats" || artwork.title == "Mille Fiori (Exterior)")) {
                     print(artwork.title!)
                     isDuplicate = true
                 }
